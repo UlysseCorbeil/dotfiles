dfe6d532ff63e22df1a3e7a6cc23ca27d4950346
Ulysse <ulysse.corbeil@isios.ca>
2022-07-18 10:09:37 -0400

    changes

--- linux/polybar/launch.sh
+++ linux/polybar/launch.sh
@@ -7,9 +7,11 @@ while pgrep -u $UID -x polyba >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload dots &
   done
 else
   polybar --reload main &
+  polybar --reload dots &
 fi
 
 # Launch the bar
